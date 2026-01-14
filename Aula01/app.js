5a32ff6242f389197c5e27824418fb0eaaecdc74
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2026-01-13 21:34:18 -0300

    a

--- Aula01/app.js
+++ Aula01/app.js
@@ -1,10 +1,12 @@
-// app.js
 import express from "express";
 
 const app = express();
-const port = 3000
+const port = 3000;
 
+app.get('/', (req, res) => {
+    res.send("Hello World!!");
+});
 
-app.get(req, res => {
-    
-})
+app.listen(port, () => {
+    console.log(`Aplicação rodando em http://localhost:${port}`);
+});
