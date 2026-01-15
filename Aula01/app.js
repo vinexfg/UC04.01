26ab70695b345f1f275d8732c0d1ebf6da005ef1
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2026-01-14 21:09:14 -0300

    a

--- Aula01/app.js
+++ Aula01/app.js
@@ -7,6 +7,19 @@ app.get('/', (req, res) => {
     res.send("Hello World!!");
 });
 
+
+app.get('/Usuario', (req, res)=> {
+    res.json({
+        nome: "Vini",
+        idade: 25,
+        cidade: "RN"
+    })
+})
+
+app.get('/status', (res, req)=> {
+    
+})
+
 app.listen(port, () => {
     console.log(`Aplicação rodando em http://localhost:${port}`);
 });
