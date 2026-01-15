1913658c7227d09bc6d69e8a0e672786f86ba3e5
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2026-01-14 21:22:04 -0300

    a

--- Aula01/app.js
+++ Aula01/app.js
@@ -7,18 +7,25 @@ app.get('/', (req, res) => {
     res.send("Hello World!!");
 });
 
-
-app.get('/Usuario', (req, res)=> {
+app.get('/Usuario', (req, res) => {
     res.json({
         nome: "Vini",
         idade: 25,
         cidade: "RN"
+    });
+});
+
+app.get('/status', (req, res) => {
+    res.status(200).send("tudo ok!");
+});
+
+app.get('/erro', (req, res) =>{
+    res.status(500).json({
+        erro: "erro interno do servidor"
     })
 })
 
-app.get('/status', (res, req)=> {
-    
-})
+app.get('/')
 
 app.listen(port, () => {
     console.log(`Aplicação rodando em http://localhost:${port}`);
