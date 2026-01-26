8471c0fc00e1cc9af409c30a66ad84d0fb1f5f9a
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2026-01-26 20:05:35 -0300

    a

--- Aula04/app.js
+++ Aula04/app.js
@@ -1,7 +1,17 @@
 import express from 'express';
-import "dotenv/config"
+import "dotenv/config";
 
 const app = express();
+const PORT = process.env.PORT;
+const mensagem = process.env.mensagem;
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).send(mensagem);
+});
+
+app.listen(PORT, () => {
+    console.log(`Aplicação rodando na porta http://localhost:${PORT}`);
+});
 
