0af9f15584caf6d8976754fa5987357081c0eee0
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2026-01-28 21:26:11 -0300

    identando codigo

--- Aula05/src/controllers/UsuarioController.js
+++ Aula05/src/controllers/UsuarioController.js
@@ -1,36 +1,37 @@
 import { UsuarioModel } from "../models/UsuarioMdeols.js";
-import axios from "axios"
+import axios from "axios";
 
-export class UsuarioController{
-    static listarUsuario(req, res){
-        try{
+export class UsuarioController {
+    static listarUsuario(req, res) {
+        try {
             const usuarios = UsuarioModel.listarUsuario();
-            if(!usuarios || usuarios.length === 0){
-                res.status(404).json({msg: "nenhum usuario cadastrado"})
-                return
+            if (!usuarios || usuarios.length === 0) {
+                res.status(404).json({ msg: "nenhum usuario cadastrado" });
+                return;
             }
-            res.status(200).json({msg: " Usuarios encontrado", usuarios})
-        }catch(error){
-            res.status(500).json({msg: "Erro ao listar usuarios", erro: error.message});
-
+            res.status(200).json({ msg: " Usuarios encontrado", usuarios });
+        } catch (error) {
+            res.status(500).json({ msg: "Erro ao listar usuarios", erro: error.message });
         }
     }
-    //funcao assincrona para buscar os dados do viacep
 
-    static async criarUsuario(req,res){
+    // funcao assincrona para buscar os dados do viacep
+    static async criarUsuario(req, res) {
         try {
-            const {nome, email, telefone, cep} = req.body;
-            if(!nome || !email || !telefone || !cep){
-                res.status(400).json({msg: "todos os campos devem ser preenchidos"})
-                return
-            } 
-            const buscarCep = await axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-            if(buscarCep.erro){
-                res.status(400).json({msg: "CEP invalido"})
-                return
+            const { nome, email, telefone, cep } = req.body;
+            if (!nome || !email || !telefone || !cep) {
+                res.status(400).json({ msg: "todos os campos devem ser preenchidos" });
+                return;
             }
-            console.log(buscarCep)
-            const novoUsuario ={
+            
+            const buscarCep = await axios.get(`https://viacep.com.br/ws/${cep}/json/`);
+            if (buscarCep.erro) {
+                res.status(400).json({ msg: "CEP invalido" });
+                return;
+            }
+            
+            console.log(buscarCep);
+            const novoUsuario = {
                 id: Date.now(),
                 nome: nome,
                 email: email,
@@ -38,13 +39,12 @@ export class UsuarioController{
                 cep: cep,
                 logradouro: buscarCep.data.logradouro,
                 uf: buscarCep.data.uf
-            }
-            const userCriado = UsuarioModel.criarUsuario(novoUsuario);
-            res.status(200).json({msg: "Usuario criado com sucesso!", userCriado});
-
-        } catch (error) {
-            res.status(500).json({msg: "Erro interno ao criar o usuario", erro: error.message})
+            };
             
+            const userCriado = UsuarioModel.criarUsuario(novoUsuario);
+            res.status(200).json({ msg: "Usuario criado com sucesso!", userCriado });
+        } catch (error) {
+            res.status(500).json({ msg: "Erro interno ao criar o usuario", erro: error.message });
         }
     }
 }
