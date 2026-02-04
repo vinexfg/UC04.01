385aa87e2d935bae4fe36bb0b394a1c348b60360
vinicius almeida <136178252+vinexfg@users.noreply.github.com>
2026-02-04 19:42:29 -0300

    q

--- Aula08/src/middlewares/authMiddleware.js
+++ Aula08/src/middlewares/authMiddleware.js
@@ -1,34 +1,71 @@
 import jwt from "jsonwebtoken";
 
-export const verificarToken = (req, res, next) => {
-    try {
+
+export function autenticarToken(req, resizeBy, next){
+ const autHeader = req.headers["authorization"]
+ //Extrair o token do header (remover o "bearer")
+
+ const token = autHeader && autHeader.split(" ")[1];
+ //se nao houver token, retorna erro 401 (nao autorizado)
+ if(!token){
+    resizeBy.status(401).json({msg: "acesso negedo - Token nao fornecido"})
+ }
+ try {
+    //verificar se o token e valido
+    const usuario = jwt.verify(token, process.env.JWT_SECRET);
+    //adiciona os dados do usuario a requisicao
+    req.usuario = usuario;
+    //continua para a proxina funcao da rota
+    next();
+ } catch (error) {
+    //se o token for invalido ou esta expirado, retorna erro 403(proibido)
+    res.status(403).json({msg: "erro interono na autorizaocao", erro: error.message })
+    
+    
+ }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const verificarToken = (req, res, next) => {
+//     try {
    
-        const authHeader = req.headers.authorization;
+//         const authHeader = req.headers.authorization;
         
-        if (!authHeader) {
-            return res.status(401).json({ error: "Token não fornecido" });
-        }
+//         if (!authHeader) {
+//             return res.status(401).json({ error: "Token não fornecido" });
+//         }
 
    
-        const token = authHeader.split(' ')[1];
+//         const token = authHeader.split(' ')[1];
         
-        if (!token) {
-            return res.status(401).json({ error: "Token não fornecido" });
-        }
+//         if (!token) {
+//             return res.status(401).json({ error: "Token não fornecido" });
+//         }
 
  
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.usuario = decoded;
+//         req.usuario = decoded;
  
-        next();
-    } catch (error) {
-        if (error.name === 'JsonWebTokenError') {
-            return res.status(401).json({ error: "Token inválido" });
-        }
-        if (error.name === 'TokenExpiredError') {
-            return res.status(401).json({ error: "Token expirado" });
-        }
-        return res.status(500).json({ error: "Erro ao verificar token" });
-    }
-};
+//         next();
+//     } catch (error) {
+//         if (error.name === 'JsonWebTokenError') {
+//             return res.status(401).json({ error: "Token inválido" });
+//         }
+//         if (error.name === 'TokenExpiredError') {
+//             return res.status(401).json({ error: "Token expirado" });
+//         }
+//         return res.status(500).json({ error: "Erro ao verificar token" });
+//     }
+// };
